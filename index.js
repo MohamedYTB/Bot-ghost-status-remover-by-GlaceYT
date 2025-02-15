2abda404cc0e56761791a45ad9bc65ab049da522
MohamedYTB <160760470+MohamedYTB@users.noreply.github.com>
2025-02-15 21:41:16 +0100

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 écoute Un Son De iNowTaG", "🎮 Regarde iNowTaG"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
